c3c71c75d78a51389d41cd4dd341755d8b998319
Kirill <kirill.nikolaevich@gmail.com>
2026-09-14 18:52:30 +0300

    change device name to my MIGHTY PLUG PRO

--- src/NUX-FS-MIDI-Client-HeltecESP32-OLED.ino
+++ src/NUX-FS-MIDI-Client-HeltecESP32-OLED.ino
@@ -38,7 +38,7 @@
 
 // Uncommenr matching device name for NUX Mighty Plug / Nux Mighty Air:
 // NUX Mighty Plug
-#define MIDI_DEVICE_NAME "NUX MIGHTY PLUG MIDI"
+#define MIDI_DEVICE_NAME "MIGHTY PLUG PRO"
 // NUX Mighty Air
 // #define MIDI_DEVICE_NAME "NUX MIGHTY AIR MIDI"
 
